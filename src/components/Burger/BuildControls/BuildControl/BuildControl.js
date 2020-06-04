f330e5aabaada03048a1e0fd55a66c0654f3ae0e
Logan McNeely <logan.k.mcneely@gmail.com>
2020-06-03 21:34:03 -0500

    Added select type to Input component

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './BuildControl.module.css';
-import classes from './BuildControl.module.css';
 
 const buildControl = (props) => (
     <div className={styles.BuildControl}>
