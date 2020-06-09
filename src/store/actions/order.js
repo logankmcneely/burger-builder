495df95424e86079759517bbfc0868450d6ed6c9
Logan McNeely <logan.k.mcneely@gmail.com>
2020-06-09 15:37:02 -0500

    +User specific orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -43,7 +43,7 @@ export const purchaseInit = () => {
 
 export const fetchOrdersSucces = (orders) => {
     return {
-        type: actionTypes.FETCH_ORDERS_FAILED,
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
         orders: orders
     }
 };
@@ -61,11 +61,13 @@ export const fetchOrdersStart = () => {
     }
 }
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        axios.get('/orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get('/orders.json' + queryParams)
             .then(res => {
+                console.log(res);
                 const fetchedOrders = [];
                 for (let key in res.data) {
                     fetchedOrders.push({
